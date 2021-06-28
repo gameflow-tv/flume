a9b84e5917ad9c480b61d07024189764d6c5dbd7
David Ramsay <david.ramsay95@gmail.com>
2021-06-28 22:58:03 +0200

    Add some criteria functionality

--- src/lib/components/Forms/TextInput/TextInput.tsx
+++ src/lib/components/Forms/TextInput/TextInput.tsx
@@ -1,48 +1,65 @@
 import { ThemeContext } from '@theme'
-import React, { useContext } from 'react'
-import styled from 'styled-components'
+import React, { useContext, useState, useRef } from 'react'
 import { Input, ListItem, Label } from './TextInput.styles'
 
+export type CriteriaRules = {
+  type: 'length' | 'regex'
+  rule: number | RegExp
+}
+
 export type TextInputCriteria = {
-  criteria: string
-  fulfilled: boolean
+  criteria?: string
+  fulfilled?: boolean
+  condition?: CriteriaRules
 }
 
 export type TextInputProps = {
   placeholder: string
   label?: string
   criteria?: TextInputCriteria[]
-  type: 'text' | 'password' | 'select'
+  type: 'text' | 'password' | 'select'  
+}
+
+const criteriaRule = (type: 'length' | 'regex', rule: number | RegExp, input: string) => {
+  if (type === 'length') {
+    if (input.length >= (rule as number)) return true
+    else return false
+  } else if (type === 'regex') {
+    const regex = rule as RegExp
+    return regex.test(input)
+  } else return false
 }
 
 export const TextInput = (props: TextInputProps) => {
   const theme = useContext(ThemeContext)
-
-return (
-
-<>
-  <Label typography={theme.typography.header5} for={`${props.type}-${props.placeholder}`}>
-    {props.placeholder.slice(0, 1).toUpperCase() + props.placeholder.slice(1).toLowerCase()}
-  </Label>
-  <br /><br />
-  <Input
-    id={`${props.type}-${props.placeholder}`}
-    typography={theme.typography.body1}
-    transition={theme.transitions.long}
-    backgroundColor={theme.colors.onBackground}
-    type={props.type}
-    placeholder={props.placeholder}
-  />
-  {props.criteria ? (
-    <ul>
-      {props.criteria.map((crit) => (
-        <ListItem
-          typography={theme.typography.body3}
-          color={crit.fulfilled ? theme.colors.success : theme.colors.primary}>
-          {crit.criteria}
-        </ListItem>
-      ))}
-    </ul>
-  ) : null}
-</>
-) }
+  const [inputValue, setInputValue] = useState('')
+  return (
+  <>
+    <Label typography={theme.typography.header5} htmlFor={`${props.type}-${props.placeholder}`}>
+      {props.placeholder.slice(0, 1).toUpperCase() + props.placeholder.slice(1).toLowerCase()}
+    </Label>
+    <br /><br />
+    <Input
+      id={`${props.type}-${props.placeholder}`}
+      typography={theme.typography.body1}
+      transition={theme.transitions.long}
+      backgroundColor={theme.colors.onBackground}
+      type={props.type}
+      placeholder={props.placeholder}
+      onChange={(e) => setInputValue(e.target.value)}
+    />
+    {props.criteria ? (
+      <ul>
+        {props.criteria.map((crit) => (
+          <ListItem
+            key={`${crit.criteria}`}
+            typography={theme.typography.body3}
+            color={criteriaRule(crit.condition.type, crit.condition.rule, inputValue) ? theme.colors.success : theme.colors.primary}>
+            {crit.criteria}
+          </ListItem>
+        ))}
+      </ul>
+    ) : null}
+  </>
+  ) 
+}
