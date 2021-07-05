1af7c2490a4b7214190271fbbca71bd5c1ad62eb
Pedro Guerrato <pguerrato@gmail.com>
2021-07-05 18:27:02 -0300

    Added Labels (ch1692)

--- src/lib/components/inputs/Slider/Slider.stories.tsx
+++ src/lib/components/inputs/Slider/Slider.stories.tsx
@@ -8,8 +8,37 @@ export default {
 }
 
 const Template = (args: SliderProps) => <Slider {...args} />
-export const Default: Story = Template.bind({})
 
-// export const Default: Story = Template.bind({
-// orientation: 'horizontal'
-// })
+export const Default: Story<SliderProps> = Template.bind({})
+Default.args = {
+  orientation: 'horizontal',
+  label: 'Label',
+  min: -100,
+  max: 100,
+  minLabel: '-',
+  maxLabel: '+'
+}
+
+export const TopLabelSlider: Story<SliderProps> = Template.bind({})
+TopLabelSlider.args = {
+  label: 'Label',
+  labelPosition: 'top'
+}
+
+export const RightLabelSlider: Story<SliderProps> = Template.bind({})
+RightLabelSlider.args = {
+  label: 'Label',
+  labelPosition: 'right'
+}
+
+export const BottomLabelSlider: Story<SliderProps> = Template.bind({})
+BottomLabelSlider.args = {
+  label: 'Label',
+  labelPosition: 'bottom'
+}
+
+export const LeftLabelSlider: Story<SliderProps> = Template.bind({})
+LeftLabelSlider.args = {
+  label: 'Label',
+  labelPosition: 'left'
+}
