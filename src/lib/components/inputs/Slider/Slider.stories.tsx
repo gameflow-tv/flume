f252dd94b7bf631bda320202a662ef235cdceaf8
Pedro Guerrato <pguerrato@gmail.com>
2021-07-05 13:59:20 -0300

    Inicial slider implemented (ch1692)

---
+++ src/lib/components/inputs/Slider/Slider.stories.tsx
@@ -0,0 +1,15 @@
+import { Story } from '@storybook/react'
+import React from 'react'
+import { Slider, SliderProps } from './Slider'
+
+export default {
+  title: 'Components/Inputs/Slider',
+  component: Slider
+}
+
+const Template = (args: SliderProps) => <Slider {...args} />
+export const Default: Story = Template.bind({})
+
+// export const Default: Story = Template.bind({
+  // orientation: 'horizontal'
+// })
---
+++ src/lib/components/inputs/Slider/Slider.tsx
@@ -0,0 +1,44 @@
+import _ from 'lodash'
+import React, { useContext } from 'react'
+import { Pats, Range, RangeProps } from './Slider.styles'
+import { ThemeContext, transitionToCss } from '../../../theme'
+import { SwitchTrackProps } from '../Switch/Switch.styles'
+
+export type SliderOrientation = 'horizontal' | 'vertical'
+
+export type SliderProps = {
+  id?: string
+  orientation?: SliderOrientation,
+  value: number,
+  min: number,
+  max: number,
+  onChange?: (value: number) => void
+}
+
+export const Slider = ({
+  id = _.uniqueId(),
+  orientation,
+  value,
+  min,
+  max,
+  onChange
+}: SliderProps) => {
+  const theme = useContext(ThemeContext)
+
+  const styles: RangeProps = {
+    trackColor: theme.colors.sliderBackground,
+    thumbColor: theme.colors.primary,
+    transition: transitionToCss(theme.transitions.short),
+  }
+
+  return (
+        <Range 
+          id={id}
+          value={value}
+          min={min}
+          max={max}
+          onChange={(e) => onChange?.call(e.target.value)}
+          {...styles}
+        />
+  )
+}
