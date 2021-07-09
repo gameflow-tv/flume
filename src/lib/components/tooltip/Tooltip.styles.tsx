ca51365d1b5798e3ce031db97efe3b40191b54ea
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-07-09 17:48:00 -0300

    prettier

--- src/lib/components/tooltip/Tooltip.styles.tsx
+++ src/lib/components/tooltip/Tooltip.styles.tsx
@@ -25,7 +25,6 @@ export const ToolTip = styled.div<TooltipProps>`
     position: absolute;
     ${(props) => setTooltipCssProps(props.position)}
     transition: all 0.3s ease;
-    
   }
 
   &::before {
@@ -37,61 +36,61 @@ export const ToolTip = styled.div<TooltipProps>`
     background-color: #000;
     width: max-content;
     max-width: 300px;
-    border-radius: ${props => props.borderRadius};
+    border-radius: ${(props) => props.borderRadius};
     text-align: center;
-    padding: 0px ${props => props.padding};
+    padding: 0px ${(props) => props.padding};
     transform: ${(props) => {
       switch (props.position) {
-      case 'left':
-        return 'translateX(-20px);'
-      case 'right':
-        return 'translateX(20px);'
-      case 'top':
-        return 'translateY(-20px);'
-      default:
-        return 'translateY(20px);'
-    }}};
+        case 'left':
+          return 'translateX(-20px);'
+        case 'right':
+          return 'translateX(20px);'
+        case 'top':
+          return 'translateY(-20px);'
+        default:
+          return 'translateY(20px);'
+      }
+    }};
     opacity: 1;
   }
 
   &::after {
-    width: ${props => props.mediumSpacing};
-    height: ${props => props.mediumSpacing};
+    width: ${(props) => props.mediumSpacing};
+    height: ${(props) => props.mediumSpacing};
     background-color: black;
     clip-path: polygon(50% 0%, 100% 50%, 50% 50%, 0% 50%);
     content: '';
     position: absolute;
     ${(props) => {
-      switch(props.position) {
-      case 'left':
-        return 'transform: rotate(90deg); left: -29%; top: 30%;'
-      case 'right':
-        return 'transform: rotate(-90deg); left: 117%; top: 30%;'
-      case 'top':
-        return 'transform: rotate(180deg); top: -40px; left: 43%;'
-      default:
-        return 'bottom: -20px; left: 43%; transform: translateY(20px);'
+      switch (props.position) {
+        case 'left':
+          return 'transform: rotate(90deg); left: -29%; top: 30%;'
+        case 'right':
+          return 'transform: rotate(-90deg); left: 117%; top: 30%;'
+        case 'top':
+          return 'transform: rotate(180deg); top: -40px; left: 43%;'
+        default:
+          return 'bottom: -20px; left: 43%; transform: translateY(20px);'
       }
     }}
     opacity: 1;
     transition: all 0.3s ease;
   }
 
-  
   &:hover::after {
     ${(props) => {
-      switch(props.position) {
-      case 'left':
-        return 'transform: translateX(20px) rotate(90deg);'
-      case 'right': 
-        return 'transform: rotate(-90deg) translateY(-20px);'
-      case 'top': 
-        return 'transform: rotate(180deg) translateY(-20px);'
-      default:
-        return 'transform: translateY(0);'
+      switch (props.position) {
+        case 'left':
+          return 'transform: translateX(20px) rotate(90deg);'
+        case 'right':
+          return 'transform: rotate(-90deg) translateY(-20px);'
+        case 'top':
+          return 'transform: rotate(180deg) translateY(-20px);'
+        default:
+          return 'transform: translateY(0);'
       }
     }};
-     opacity: 1;   
+    opacity: 1;
   }
 
   &:hover::before {
