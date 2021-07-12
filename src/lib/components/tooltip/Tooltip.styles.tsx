b04f1f2ce28ee71f88dfe839b564fabb2226b81c
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-07-12 12:10:16 -0300

    prettier

--- src/lib/components/tooltip/Tooltip.styles.tsx
+++ src/lib/components/tooltip/Tooltip.styles.tsx
@@ -53,7 +53,6 @@ export const ToolTip = styled.div<TooltipProps>`
     opacity: 0;
   }
 
-  
   &::after {
     width: ${(props) => props.mediumSpacing};
     height: ${(props) => props.mediumSpacing};
@@ -76,7 +75,6 @@ export const ToolTip = styled.div<TooltipProps>`
     opacity: 0;
     transition: all 0.3s ease;
   }
-
 
   &:hover::after {
     ${(props) => {
