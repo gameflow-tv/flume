04e0c891a701df8b9b33cfaa76d442cbafa3b81a
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-07-13 12:32:43 -0300

    create icon button

---
+++ src/lib/components/LikeButton/LikeButton.styles.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { LikeButtonProps } from './LikeButton'
+
+export const LikeIcon = styled.button<LikeButtonProps>`
+    width: 42px;
+    height: 42px;
+    border-radius: ${props => props.borderRadius};
+    box-shadow: ${props => props.liked ? props.xSmallShadow : props.smallShadow};
+    background: #525556;
+    border: unset;
+    font-size: ${props => props.fontSize};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${(props) => props.liked ? props.color : props.neutralColor};
+    filter: brightness(80%);
+    transition: all .3s ease;
+
+    &:hover {
+        filter: brightness(100%);
+    }
+
+    &:disabled {
+        background: #36393B;
+        box-shadow: 0px 2px 6.4px rgba(0, 0, 0, 0.1);
+        color: #eeeeee8b;
+        
+    }
+
+
+`
