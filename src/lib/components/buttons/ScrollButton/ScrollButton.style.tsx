bba30a39a9363d95e00b5f0552d2b0ca210f467f
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-07-14 10:48:59 -0300

    prettier

--- src/lib/components/buttons/ScrollButton/ScrollButton.style.tsx
+++ src/lib/components/buttons/ScrollButton/ScrollButton.style.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
-import {ScrollButtonProps} from './ScrollButton'
+import { ScrollButtonProps } from './ScrollButton'
 
 export const Button = styled.button<ScrollButtonProps>`
   width: 55px;
   height: 55px;
-  border-radius: ${props => props.borderRadius};
+  border-radius: ${(props) => props.borderRadius};
   background: linear-gradient(180deg, #525556 0%, #434647 100%);
   border: unset;
-  box-shadow: ${props => props.boxShadowSmall};
+  box-shadow: ${(props) => props.boxShadowSmall};
   font-size: 21px;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   cursor: pointer;
   filter: brightness(80%);
   transition: all 0.3s ease;
@@ -29,12 +29,12 @@ export const Button = styled.button<ScrollButtonProps>`
 
   &:active {
     background: linear-gradient(180deg, #3a3c3d 0%, #2b2d2e 100%);
-    box-shadow: ${props => props.boxShadowXSmall};
+    box-shadow: ${(props) => props.boxShadowXSmall};
   }
 
   &:disabled {
     background: #434647;
-    box-shadow: ${props => props.boxShadowSmall};
+    box-shadow: ${(props) => props.boxShadowSmall};
     color: rgba(255, 255, 255, 0.5);
   }
 `
