eca880ac64e7c31d0e2baa8aaf12f79cf816cfdc
Arne Molland <arne@molland.sh>
2021-07-15 19:42:17 +0200

    Revert "prettier"
    This reverts commit b5fa09cef5be32eb6f462a75ec70ce9e5983eb02.

--- src/lib/components/buttons/ScrollButton/ScrollButton.tsx
+++ src/lib/components/buttons/ScrollButton/ScrollButton.tsx
@@ -5,20 +5,21 @@ import { ThemeContext } from '../../../theme'
 import { Button } from './ScrollButton.style'
 
 export type ScrollButtonProps = {
-  boxShadowSmall: string
-  boxShadowXSmall: string
-  borderRadius: string
-  color: string
+    boxShadowSmall: string
+    boxShadowXSmall: string
+    borderRadius: string
+    color: string
 }
 
 export const ScrollButton = () => {
-  const theme = useContext(ThemeContext)
+    const theme = useContext(ThemeContext)
   return (
     <Button
-      boxShadowXSmall={theme.shadows.xsmall}
-      boxShadowSmall={theme.shadows.small}
-      borderRadius={theme.shapes.borders.small}
-      color={theme.colors.primaryText}>
+    boxShadowXSmall={theme.shadows.xsmall}
+    boxShadowSmall={theme.shadows.small}
+    borderRadius={theme.shapes.borders.small}
+    color={theme.colors.primaryText}
+    >
       <FontAwesomeIcon icon={faArrowRight} />
     </Button>
   )
