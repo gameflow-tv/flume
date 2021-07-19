faa27772e68fe0c578496dd3c69078d38d6e150c
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-07-19 09:00:14 -0300

    remove unused core

--- src/lib/components/Loading/LoadingComponents/Animation/Shimmer.styles.tsx
+++ src/lib/components/Loading/LoadingComponents/Animation/Shimmer.styles.tsx
@@ -14,18 +14,3 @@ export const loading = () => {
   `
 }
 
-export const ShimmerWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`
-
-export const Slide = styled.div`
-  width: 50%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.05);
-  transform: skewX(-20deg);
-  box-shadow: 0 0 30px 30px rgba(255, 255, 255, 0.05);
-`
