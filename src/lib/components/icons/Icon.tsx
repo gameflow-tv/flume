025a08eddcc7e5ae71034fcbf18c5d34770e5dfe
Arne Molland <arne@molland.sh>
2021-11-05 15:51:37 +0100

    Add size, inline, filled props to Icon

--- src/lib/components/icons/Icon.tsx
+++ src/lib/components/icons/Icon.tsx
@@ -3,23 +3,50 @@ import styled from 'styled-components'
 import { useTheme } from '../../hooks'
 import { IconData, Icons } from './data'
 
+export const IconSize = {
+  small: '0.75rem',
+  medium: '1rem',
+  large: '1.5rem',
+  xlarge: '2rem'
+}
+
 export type IconProps = {
   icon?: keyof Icons | string
   color?: string
+  size?: keyof typeof IconSize | string
+  inline?: boolean
+  filled?: boolean
 }
 
-const _Icon = styled.div`
-  width: 24px;
-  height: 24px;
+const _Icon = styled.div<{ size?: string; inline?: boolean }>`
+  display: inline-flex;
+  align-self: center;
+  width: ${(props) => props.size ?? IconSize.medium};
+  height: ${(props) => props.size ?? IconSize.medium};
+
+  & > svg {
+    font-size: ${(props) => (props.inline ? 'inherit' : props.size ?? IconSize.medium)};
+  }
 `
 
-export const Icon = ({ icon, color }: IconProps) => {
+export const Icon = ({ icon, color, size, inline, filled }: IconProps) => {
   const theme = useTheme()
 
+  let s
+
+  if (size in IconSize) {
+    s = IconSize[size]
+  } else {
+    s = size
+  }
+
+  if (filled && !icon.includes('filled') && icon + '_filled' in IconData) {
+    icon += '_filled'
+  }
+
   return (
-    <_Icon>
+    <_Icon size={s} inline={inline}>
       <svg
-        data-name="Layer 1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill={color ? color : theme.colors.primaryText}
