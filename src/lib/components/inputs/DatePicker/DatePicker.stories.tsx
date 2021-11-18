83641c98c6f17ba0ed6c265e09640132c93a82d5
Pedro Guerrato <pguerrato@gmail.com>
2021-11-18 09:44:25 -0300

    Improved DateTime Picker (#26)
    * Improved component behaviour and corrected data propagation (sc2262)
    
    * Improved DatePicker behaviour component (sc2262)

--- src/lib/components/inputs/DatePicker/DatePicker.stories.tsx
+++ src/lib/components/inputs/DatePicker/DatePicker.stories.tsx
@@ -1,5 +1,5 @@
 import { Story } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { DatePicker, DatePickerProps } from './DatePicker'
 
 export default {
@@ -11,3 +11,15 @@ const Template = (args: DatePickerProps) => <DatePicker {...args} />
 
 export const Default: Story = Template.bind({})
 Default.args = {}
+
+export const ExternalContol: Story = (args: DatePickerProps) => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  return (
+    <div>
+      <label style={{ color: 'white' }}>{selectedDate.toISOString()}</label>
+      <br />
+      <br />
+      <DatePicker onDateChange={setSelectedDate} />
+    </div>
+  )
+}
