e29bdb8dabe0bcb6392c454bea958c687e50f70a
Pedro Guerrato <pguerrato@gmail.com>
2021-11-18 13:51:59 -0300

    Removed temporaly modals (sc2181)

--- src/lib/components/modals/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ModalHeader } from './modalHeader/ModalHeader'
-export { default as ModalBody } from './modalBody/ModalBody'
-export { default as ModalFooter } from './modalFooter/ModalFooter'
-export * from './Modal/Modal'
