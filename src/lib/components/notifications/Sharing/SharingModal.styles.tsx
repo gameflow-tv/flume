a2eab8f1be5afd8eed68f8ce4fad621f4b98e05e
Pedro Guerrato <pguerrato@gmail.com>
2021-11-22 10:22:45 -0300

    Added styles ModalBody

--- src/lib/components/notifications/Sharing/SharingModal.styles.tsx
+++ src/lib/components/notifications/Sharing/SharingModal.styles.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components'
 import { typographyToCss } from '../../../theme'
 import theme from '../../../theme/theme'
-import { ModalHeader } from '../Modal'
+import { ModalBody, ModalHeader } from '../Modal'
 
 export const SharingHeader = styled(ModalHeader)`
   ${typographyToCss(theme.typography.header1)};
   font-size: 1.5rem;
   font-weight: normal;
   margin-top: 28px;
+`
+
+export const SharingBody = styled(ModalBody)`
+  margin-top: 32px;
+  margin-bottom: 28px;
 `
--- src/lib/components/notifications/Sharing/SharingModal.tsx
+++ src/lib/components/notifications/Sharing/SharingModal.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Modal, ModalBody } from '../Modal'
-import { SharingHeader } from './SharingModal.styles'
+import { Modal } from '../Modal'
+import { SharingBody, SharingHeader } from './SharingModal.styles'
 
 export const SharingModal = () => {
   return (
     <Modal show={true} size="546px">
       <SharingHeader>Share link</SharingHeader>
-      <ModalBody>asdfasdfasdf</ModalBody>
+      <SharingBody>asdfasdfasdf</SharingBody>
     </Modal>
   )
 }
