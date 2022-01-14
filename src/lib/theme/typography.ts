4f1525a4298f93de0640c5b21274af4c9d2b6ab8
Arne Molland <arne@molland.sh>
2022-01-14 11:02:26 +0100

    Add mobileFontSize to typography

--- src/lib/theme/typography.ts
+++ src/lib/theme/typography.ts
@@ -1,3 +1,6 @@
+// TODO: Get breakpoints from theme context
+import * as breakpoints from './breakpoints'
+
 export type FontFamily = 'Montserrat' | 'Mulish'
 
 export const typographyToCss = (style: TypographyStyle, overrides?: Partial<TypographyStyle>) => `
@@ -6,6 +9,16 @@ export const typographyToCss = (style: TypographyStyle, overrides?: Partial<Typo
   font-weight: ${overrides && overrides.fontWeight ? overrides.fontWeight : style.fontWeight};
   line-height: ${overrides && overrides.lineHeight ? overrides.lineHeight : style.lineHeight};
   margin-block: 0;
+
+  @media (max-width: ${breakpoints.default.sm}) {
+    font-size: ${
+      overrides && overrides.fontSize
+        ? overrides.fontSize
+        : style.mobileFontSize
+        ? style.mobileFontSize
+        : style.fontSize
+    };
+  }
 `
 
 export type TypographyStyle = {
@@ -13,6 +26,7 @@ export type TypographyStyle = {
   fontSize: string
   lineHeight: number
   fontWeight: number
+  mobileFontSize?: string
 }
 
 export type Typography = {
@@ -32,25 +46,29 @@ const fallback: Typography = {
     fontSize: '2.25rem', // '36px'
     lineHeight: 1.2,
     fontWeight: 600,
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    mobileFontSize: '1.5rem'
   },
   header2: {
     fontSize: '1.5rem', // '24px'
     lineHeight: 1.2,
     fontWeight: 500,
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    mobileFontSize: '1.125rem'
   },
   header3: {
     fontSize: '1.125rem', // '18px'
     lineHeight: 1.2,
     fontWeight: 600,
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    mobileFontSize: '1rem'
   },
   header4: {
     fontSize: '1rem', // '16px'
     lineHeight: 1.2,
     fontWeight: 500,
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    mobileFontSize: '0.875rem'
   },
   header5: {
     fontSize: '0.875rem', // '14px'
