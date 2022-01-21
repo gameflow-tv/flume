001febef58febdfd833b68a5c106bc70d2705f34
Arne Molland <arne@molland.sh>
2022-01-21 19:47:36 +0100

    Export scroll button

--- src/lib/components/buttons/index.ts
+++ src/lib/components/buttons/index.ts
@@ -1,5 +1,6 @@
 export * from './Button'
 export * from './IconButton'
 export * from './LikeButton'
+export * from './ScrollButton'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'signal'
