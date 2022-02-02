30b76ea318b3a3ac758e97ad478e8654d553b752
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2022-02-02 09:59:25 -0300

    change tooltip distance

--- src/lib/components/common/TooltipAlt/TooltipAlt.stories.tsx
+++ src/lib/components/common/TooltipAlt/TooltipAlt.stories.tsx
@@ -11,8 +11,35 @@ export default {
 const Template = (props: TooltipAltProps) => {
   return (
     <TooltipAlt label="This is a tooltip" position="top">
-      <div style={{ height: '50px', width: '50px', border: '1px solid red' }}>I am target</div>
+      <div style={{ height: '50px', width: '50px', backgroundColor: 'black' }}>I am target</div>
     </TooltipAlt>
   )
 }
 export const Default: Story = Template.bind({})
+
+const Bottom = (props: TooltipAltProps) => {
+  return (
+    <TooltipAlt label="This is a tooltip" position="bottom">
+      <div style={{ height: '100px', width: '100px', backgroundColor: 'black' }}>I am target</div>
+    </TooltipAlt>
+  )
+}
+export const BottomTooltip: Story = Bottom.bind({})
+
+const Left = (props: TooltipAltProps) => {
+  return (
+    <TooltipAlt label="This is a tooltip" position="left">
+      <div style={{ height: '100px', width: '100px', backgroundColor: 'black' }}>I am target</div>
+    </TooltipAlt>
+  )
+}
+export const LeftTooltip: Story = Left.bind({})
+
+const Right = (props: TooltipAltProps) => {
+  return (
+    <TooltipAlt label="This is a tooltip" position="right">
+      <div style={{ height: '100px', width: '100px', backgroundColor: 'black' }}>I am target</div>
+    </TooltipAlt>
+  )
+}
+export const RightTooltip: Story = Right.bind({})
