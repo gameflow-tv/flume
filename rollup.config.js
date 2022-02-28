06bf650e94537fa4b4c2db621373bb28b0419640
Arne Molland <arne@molland.sh>
2022-02-28 21:32:26 +0100

    package: compile sass separately

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from '@rollup/plugin-typescript'
-import scss from 'rollup-plugin-scss'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -19,9 +18,6 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
-    scss({
-      outputStyle: 'compressed'
-    })
   ],
   preserveModules: true
 }
