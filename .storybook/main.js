9d877b04182a7fe5d7505f8cc908ce41273aeb56
Arne Molland <arne@molland.sh>
2022-03-02 09:54:36 +0100

    storybook: disable mdxv2 preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(tsx|mdx)', '../docs/**/*.stories.@(js|ts|jsx|tsx|mdx)'],
   addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
     '@storybook/addon-a11y',
     '@storybook/addon-controls',
     '@storybook/addon-backgrounds',
     '@storybook/addon-storysource',
-    '@storybook/addon-docs',
     '@storybook/preset-scss'
   ],
   typescript: {
@@ -13,8 +14,5 @@ module.exports = {
   },
   core: {
     builder: 'webpack5'
-  },
-  features: {
-    previewMdx2: true
   }
 }
