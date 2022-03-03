412708bdb82d8ed4b9381545ea5cabab1c3a6003
Arne Molland <arne@molland.sh>
2022-03-03 14:13:10 +0100

    all: fix fallback imports

--- src/lib/theme/ThemeProvider.tsx
+++ src/lib/theme/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import fallback, { Theme } from './theme'
+import { fallbackTheme, Theme } from './theme'
 import React, { ReactNode } from 'react'
 
 export type ThemeContextProps = {
@@ -6,9 +6,12 @@ export type ThemeContextProps = {
   children?: ReactNode
 }
 
-export const ThemeContext: React.Context<Theme> = React.createContext(fallback)
+export const ThemeContext: React.Context<Theme> = React.createContext(fallbackTheme)
 
-export const ThemeProvider = ({ theme = fallback, children }: ThemeContextProps): JSX.Element => (
+export const ThemeProvider = ({
+  theme = fallbackTheme,
+  children
+}: ThemeContextProps): JSX.Element => (
   <ThemeContext.Provider value={theme}>
     <React.Fragment>{children}</React.Fragment>
   </ThemeContext.Provider>
