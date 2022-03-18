3273ca17512ef3fd102bb39122cd2c6493cb10b0
Pedro Guerrato <pguerrato@gmail.com>
2022-03-18 09:59:35 -0300

    Implement marquee component  (#71)
    * Create the basic for marquee (sc2156)
    
    * Finish marquee creation (sc2156)
    
    * Format Marquee stories file (sc2156)
    
    * Add functionality to slide when mouse is over (sc2156)
    
    * Improve names on stories (sc2156)
    
    * Export Marquee (sc2156)
    
    * Remove left padding on Marquee (sc2156)
    
    * Remove global variation addition (sc2156)

---
+++ src/lib/components/common/Marquee/Marquee.styles.tsx
@@ -0,0 +1,94 @@
+import styled, { keyframes } from 'styled-components'
+
+export type MarqueeStyles = {
+  duration: number
+  width?: string
+}
+
+const slideMainWhenStart = (props) => {
+  return keyframes`
+    0% {
+      transform: 0px;
+    }
+
+    100% {
+      transform: translateX(-100%);
+    }
+  `
+}
+
+const slideMainForever = (props) => {
+  return keyframes`
+    0% {
+      transform:  translateX(100%);
+    }
+
+    100% {
+      transform: translateX(-100%);
+    }
+  `
+}
+
+const slideFakeForever = (props) => {
+  return keyframes`
+    0% {
+      transform:  translateX(0);
+    }
+
+    100% {
+      transform: translateX(-200%);
+    }
+  `
+}
+
+export const Track = styled.div<MarqueeStyles>`
+  --duration: ${(props) => props.duration}s;
+  box-sizing: border-box;
+  width: ${(props) => props.width || '100%'};
+  overflow: hidden;
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export const Roller = styled.div.attrs((props) => ({
+  'aria-hidden': true
+}))`
+  display: block;
+  width: max-content;
+  min-width: 200%;
+`
+
+export const MainContent = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0 20px 0 0;
+  width: fit-content;
+  min-width: 50%;
+  &.slide {
+    animation-name: ${slideMainWhenStart}, ${slideMainForever};
+    animation-duration: var(--duration), calc(var(--duration) * 2);
+    animation-delay: 0s, var(--duration);
+    animation-timing-function: linear, linear;
+    animation-iteration-count: 1, infinite;
+  }
+`
+
+export const FakeContent = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0 20px 0 0;
+  width: fit-content;
+  min-width: 50%;
+  &.slide {
+    animation-name: ${slideFakeForever};
+    animation-duration: calc(var(--duration) * 2);
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-delay: 0;
+  }
+`
